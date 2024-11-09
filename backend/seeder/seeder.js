934ae0305b828da8e9e95bcd5d230e77113bbc19
Stella <stellabs.2011@gmail.com>
2024-11-09 18:01:40 -0300

    deploy

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -5,7 +5,7 @@ import Product from "../models/product.js";
 const seedProducts = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://ghulamabbas:ghulamabbas@shopit.wkd8107.mongodb.net/shopit-v2?retryWrites=true&w=majority"
+      "mongodb+srv://stellabs2011:100820015@cluster0.yiisg.mongodb.net/shopit-v2?retryWrites=true&w=majority&appName=Cluster0"
     );
 
     await Product.deleteMany();
